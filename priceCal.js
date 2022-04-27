5240521542efda65d1dfcd0c1935a5dd95a56399
Natcha <2010511101008@live4.utcc.ac.th>
2022-04-27 18:53:11 +0700

    Revise priceCal

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,18 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
+  var tp;
+	if (eventId == 1){
+		tp = ticknum * 91.25;
+	}else if (eventId == 2){
+		tp = ticknum * 79.88;
+	}else if (eventId == 3){
+		tp = ticknum * 83.12;
+	}else if (eventId == 4){
+		tp = ticknum * 100;
+	}
   
-  return 0;
+  return tp;
 }
 
 
